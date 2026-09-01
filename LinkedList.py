4b99a12cf80fac13dbf43b290446fadf11f21165
batistedecat <batiste.decat@icloud.com>
2026-09-01 20:55:24 +0200

    basic functionality, not yet tested

--- LinkedList.py
+++ LinkedList.py
@@ -20,29 +20,51 @@ class Node:
         self.prev = prev
 
 class LinkedList: #Doubly linked
-    def __init__(self, r = None, t = None):
-        self.head = r
-        self.tail = t
+    def __init__(self, head = None):
+        self.head = head
         self.size = 0
-    #accessors
     def search(self, data):
-        pass
+        curr = self.head
+        while curr:
+            if curr.get_data() == data:
+                return curr
+            curr = curr.get_next()
+        return None
     def length(self):
         return self.size
     #mutators
-
-
-    def Front_add(self, new_data): #few steps. still need for onother add??
-        new_node = Node(new_data)
-        if self.head:
+    def add(self, new_data): #few steps. still need for onother add??
+        new_node = Node(new_data, next = self.head)
+        if self.head is not None:
             self.head.set_prev(new_node)
         self.head = new_node
         self.size += 1
-    def Rear_add(self, new_data):
-        pass
-
 
     def remove(self, data):
-        pass
+        curr = self.root
+        while curr:
+            if curr.get_data() = data:
+                next = curr.get_next()
+                prev = curr.get_prev()
 
+                if next:
+                    next.set_prev(prev)
+                if prev:
+                    prev.set_next(next)
+                else: #then the this node is the head of the chain, use its next node as new head
+                    self.head = next
+                self.size -= 1
+                return True #succesful deletion
+            else:
+                curr = curr.get_next()
+        return False #if no such node is found
+
+
+    def __str__(self):
+        curr = []
+        this_node = self.head
+        for i in range(self.size):
+            curr.append(this_node.data)
+            this_node = this_node.next
+        return ', '.join(curr)
 

f07bebba836c40da9d48b71b2460f8d7d36d4cd3
batistedecat <batiste.decat@icloud.com>
2026-09-01 18:01:31 +0200

    accessors and mutators

--- LinkedList.py
+++ LinkedList.py
@@ -3,4 +3,21 @@ class Node:
         self.data = data
         self.next = next
         self.prev = prev
+    #accessors
+    def get_data(self):
+        return self.data
+    def get_next(self):
+        return self.next
+    def get_prev(self):
+        return self.prev
+
+    #mutators
+    def set_data(self,data):
+        self.data = data
+    def set_next(self,next):
+        self.next = next
+    def set_prev(self,prev):
+        self.prev = prev
+
+
 
